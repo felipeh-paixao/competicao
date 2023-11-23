fdade3d5e93591b2e57c51b12c775c739ef833f8
Luffy <felipe_infa@live.com>
2023-11-23 19:11:18 -0300

    Quase folga

--- Justificador.py
+++ Justificador.py
@@ -1,19 +1,18 @@
 n = int(input())
 while n != 0:
     nomes = []
-    tamanho = 0
+    tamanho = 0   
+
     for i in range(n):        
-        nomes.append(input().strip())
+        nomes.append(input())
         if len(nomes[i]) > tamanho:
             tamanho = len(nomes[i])
     
     for nome in nomes:
-        print(' ' * (tamanho - len(nome)) + nome)
-        
-    print()
-
-    n = int(input())    
+        nome = ' ' * (tamanho - len(nome)) + nome
+        print(nome)        
     
-
-
-           
+    n = int(input())
+    if(n != 0):
+        print()  
+        

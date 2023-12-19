25d9e1a86ec9381b5c37cdc1e4db1f06b33b01e8
Luffy <felipe_infa@live.com>
2023-12-19 20:11:58 -0300

    ta acabando

--- target_practice.py
+++ target_practice.py
@@ -3,28 +3,21 @@ cases = int(input())
 for x in range(cases):
     pontuacao = 0
     rings = []
-    
     for k in range(10):
         rings.append(input())
-    cont = 1
+    
     for i in range(10):
-        for j in range(10):
-            print(rings[i][j])
-            if i <= 4:
-                z = i
-            else:
-                z = i - cont
-                cont += 1
+        for j in range(10):          
 
-            if(z == 0 or j == 0) and rings[i][j] != '.':
+            if(i == 0 or j == 0 or j == 9 or i == 9) and rings[i][j] != '.':
                 pontuacao += 1
-            elif z == 1  and 0 < j < 9 and  rings[i][j] != '.':
+            elif(i == 1 or i == 8 or j == 1 or j == 8)  and  rings[i][j] != '.':
                 pontuacao += 2
-            elif z == 2 and 1 < j < 8 and rings[i][j] != '.':
+            elif(i == 2 or i == 7 or j == 2 or j == 7) and rings[i][j] != '.':
                 pontuacao += 3
-            elif z == 3 and 2 < j < 7 and rings[i][j] != '.':
+            elif (i == 3 or i == 6 or j == 3 or j == 6) and rings[i][j] != '.':
                 pontuacao += 4
-            else:
+            elif rings[i][j] != '.':
                 pontuacao += 5
             
     

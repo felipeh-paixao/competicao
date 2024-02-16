0b7f210aa611eed39f7fe8108aaa79259deb762f
Luffy <felipe_infa@live.com>
2024-02-16 12:57:00 -0300

    cheio de patin

--- cf_1300/CF165A.py
+++ cf_1300/CF165A.py
@@ -1,5 +1,4 @@
 n = int(input())
-parada = n * 2
 pontos = []
 contador = 0
 i = 0
@@ -9,4 +8,24 @@ while(i < n):
     pontos.append(vet)
     i += 1
 pontos.sort()
-print(pontos)
+
+for ponto in pontos:
+  maior_y = 0
+  menor_y = 0
+  maior_x = 0
+  menor_x = 0
+  for j in range(n):     
+    if ponto[0] == pontos[j][0]:      
+      if ponto[1] > pontos[j][1] and maior_y == 0:
+        maior_y += 1        
+      if ponto[1] < pontos[j][1] and menor_y == 0:
+        menor_y += 1
+    if ponto[1] == pontos[j][1]:      
+      if ponto[0] > pontos[j][0] and maior_x == 0:
+        maior_x = 1
+      if ponto[0] < pontos[j][0] and menor_x == 0:
+        menor_x = 1
+  if maior_x + maior_y + menor_x + menor_y == 4:  
+      contador += 1
+
+print(contador)

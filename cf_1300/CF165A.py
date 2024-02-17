d7100f4815c851087143516c4037c2aac3794d46
Luffy <felipe_infa@live.com>
2024-02-17 12:24:33 -0300

    essa foi bronca

--- cf_1300/CF165A.py
+++ cf_1300/CF165A.py
@@ -4,7 +4,7 @@ contador = 0
 i = 0
 while(i < n):
     linha = input().split()
-    vet = [linha[0], linha[1]]
+    vet = [int(linha[0]), int(linha[1])]
     pontos.append(vet)
     i += 1
 pontos.sort()
@@ -16,16 +16,16 @@ for ponto in pontos:
   menor_x = 0
   for j in range(n):     
     if ponto[0] == pontos[j][0]:      
-      if ponto[1] > pontos[j][1] and maior_y == 0:
-        maior_y += 1        
-      if ponto[1] < pontos[j][1] and menor_y == 0:
+      if ponto[1] > pontos[j][1]:
+        maior_y = 1        
+      if ponto[1] < pontos[j][1]:
         menor_y += 1
     if ponto[1] == pontos[j][1]:      
-      if ponto[0] > pontos[j][0] and maior_x == 0:
+      if ponto[0] > pontos[j][0]:
         maior_x = 1
-      if ponto[0] < pontos[j][0] and menor_x == 0:
+      if ponto[0] < pontos[j][0]:
         menor_x = 1
-  if maior_x + maior_y + menor_x + menor_y == 4:  
+  if maior_x and maior_y and menor_x and menor_y:  
       contador += 1
 
 print(contador)
